024ea6642c2f41966f139c720a8dd659fdb22e0b
adityayadav7352 <Adityayadav7352@gmail.com>
2022-02-26 02:49:44 +0530

    feat : add data extract

---
+++ sample.java
@@ -0,0 +1,20 @@
+package webcrabel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test1 {
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","A:\\kyc project\\webcrabel\\src\\driver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
+	
+	}
+
+}
